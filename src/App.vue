21136ed0ffb4e56661222f28ba818e79ad9d6272
said-jaouad <jaouad_sd@hotmail.com>
2023-01-30 15:03:14 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import 'font-awesome/css/font-awesome.css'
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <header>
+    <Home/>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
